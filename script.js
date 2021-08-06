c1201137996013e6251d8eed832600287a603af7
Anurag <anuragqwerty09@outlook.com>
2021-08-06 20:37:41 +0530

    mobile design done

---
+++ script.js
@@ -0,0 +1,15 @@
+"use strict"
+
+const faqs = document.querySelectorAll(".faq");
+console.log(faqs);
+faqs.forEach(faq=>{
+    faq.addEventListener("click",()=> {     
+       if(faq.classList.contains("active")){
+        faq.classList.toggle("active");
+       }
+        else{
+            faqs.forEach(faq=>faq.classList.remove("active"))
+            faq.classList.add("active");
+        }
+    });
+});
